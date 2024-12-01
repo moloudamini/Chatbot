dfa38993f94bb5f8e73142d8bd3bc1727e971f6a
sarahamini <sarah.amini@tripstack.com>
2024-12-01 13:32:32 -0500

    update-frontend

--- chatbot-frontend/src/index.tsx
+++ chatbot-frontend/src/index.tsx
@@ -1,8 +1,25 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+// Create the router with the v7_startTransition
+const router = createBrowserRouter(
+  [
+    {
+      path: "*",
+      element: <App />,
+    },
+  ],
+  {
+    future: {
+      v7_startTransition: true,
+    },
+  }
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+
+root.render(<RouterProvider router={router} />);
